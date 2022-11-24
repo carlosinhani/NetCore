423b1faca332ddf52e7196532010afaffb55b1fa
Carlos.inhani <carlos.inhani@gmail.com>
2022-11-24 08:53:53 -0300

    ContextFactory

---
+++ src/Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            //Usado para Criar as Migrações
+            var connectionString = "Serve=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=mudar@123";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
